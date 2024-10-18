f75cb92b7fb62504f439ba9dada7aef6017ef6db
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-18 17:55:12 +0700

    fix product_variant

--- controllers/order.js
+++ controllers/order.js
@@ -5,14 +5,16 @@ const prisma = new PrismaClient();
 
 app.post("/product", async (req,res)=>{
     try{
-
+        console.log(req.body)
+        const id = req.body.id
         const product = await prisma.product.findMany({
             skip: 0,
             take: 1,
             include: {
+                // where: {id: id},
                 product_detail: true,
                 img_url: {where: {}},
-                // category: true
+                variants: {}
             }
         })
         res.send(product)
