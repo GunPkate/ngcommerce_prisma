30ab7ed5e97e9e58eec2757e49988d31c7f1a715
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-18 13:45:37 +0700

    api productlist & random item

--- controllers/product.js
+++ controllers/product.js
@@ -5,10 +5,17 @@ const prisma = new PrismaClient();
 
 app.post("/selectProduct", async (req,res)=>{
     try{
+        let count = await prisma.product.count()
+        let item = 6
+        if(req.body.page == ''){
+            item = 3
+        }
+        let skip = Math.floor(Math.random()*count) 
+        if(skip > count - item ){ skip = 0}
 
         const product = await prisma.product.findMany({
-            skip: 0,
-            take: 6,
+            skip: skip,
+            take: item,
             include: {
                 // variants: true,
                 // details: true,
